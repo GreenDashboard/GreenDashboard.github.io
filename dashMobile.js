ef7dbf250f4fa49cbbc34170bdbbd4b81401af90
JHawke <jh17594@bristol.ac.uk>
2022-03-23 20:59:12 +0000

    Bug fix

--- dashMobile.js
+++ dashMobile.js
@@ -988,8 +988,7 @@ var houseID = {
       chart3.update();
     }
   
-  
-    document.getElementById("5 YearsM").click();
+
   
   
   };
@@ -1388,27 +1387,6 @@ var houseID = {
     "December",
   ]
   
-  document.getElementById("5 YearsM").click();
-  function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  document.getElementById("defaultOpenM").click();
+
   
   
